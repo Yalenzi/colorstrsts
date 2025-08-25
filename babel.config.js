2bc5470c068fe2bdaf69c94eab80f03bd343eba8
Yalenzi <ararsmomarar@gmail.com>
2025-08-26 01:28:15 +0300

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-// This file exists to prevent Next.js from using Babel
-// Next.js will use SWC by default when this file is not present
-// But since some tools might create it, we keep it minimal
-
-module.exports = {
-  presets: ['next/babel']
-};
